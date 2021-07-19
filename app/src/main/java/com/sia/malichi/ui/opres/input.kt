c30443608c324c10460969373fadb8df886274fb
VAIO <siavash.alborzi@gmail.com>
2021-07-19 13:32:28 +0430

    initializing,first commit

--- app/src/main/java/com/sia/malichi/ui/opres/input.kt
+++ app/src/main/java/com/sia/malichi/ui/opres/input.kt
@@ -1,10 +1,11 @@
 package com.sia.malichi.ui.opres
 
+//ساخت آرایه ماتریسی توسط کاربر با تعداد شطر و ستون دلخواه
+//enter data by user , custom matrix , price or quantity
 
-// ورودی تعداد سطر و ستون جدول پژوهش
-// number of Row and Column input by user
 
 val table = {
     var x
     var y
 }
+
